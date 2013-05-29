988eb671f2159a03ec54a379a6c12b4972dfd5ff
David Nix <david@threadlife.com>
2013-05-28 23:13:19 -0600

    Initial commit.

---
+++ cache_helper.rb
@@ -0,0 +1,89 @@
+require 'active_support/concern'
+
+module CacheHelper
+  extend ActiveSupport::Concern
+
+  included do
+    attr_accessor :prevent_caching
+    after_save :cache_thyself
+    after_destroy :delete_from_cache
+  end
+
+  def cache_thyself(cache_related_models=true)
+    return if prevent_caching?
+    to_cache = self._we_cache_this.merge(type: class_type)
+    RedisHelper.with_connection do |conn|
+      conn.set(self.redis_cache_key, to_cache.to_json)
+    end
+    true
+  end
+
+  def _we_cache_this
+    self.as_json
+  end
+
+  def redis_cache_key
+    CacheHelper.cache_key_for(self)
+  end
+
+  def self.cache_key_for(arg, uid=nil)
+    if arg.is_a?(Class)
+      return nil if uid.nil?
+      "#{arg.to_s.underscore}-#{uid}"
+    else # instance of class
+      "#{arg.class.to_s.underscore}-#{arg.id}"
+    end
+  end
+
+  def delete_from_cache
+    RedisHelper.del(self.redis_cache_key)
+  end
+
+  def cached_json
+    RedisHelper.get(self.redis_cache_key)
+  end
+
+  def info_hash_via_cache(current_user=nil)
+    CacheHelper.json_hash(self.class, self.id, current_user)
+  end
+
+  # Pull json out of redis w/o needing instance of the object
+  # Fall back to db if can't find cached data
+  def self.json_hash(klass, uid, current_user=nil)
+    return {} if klass.nil? || uid.nil?
+    key = cache_key_for(klass, uid)
+    json_string = RedisHelper.get(key)
+    final_hash = {}
+    if json_string.present?
+      final_hash = JSON.parse(json_string)
+    elsif klass.ancestors.include?(ActiveRecord::Base)
+      final_hash = klass.find(uid).as_json
+    end
+    indifferent_hash(final_hash)
+  end
+
+  def hash_via_db(user)
+    if self.is_a?(User)
+      self.condensed_information(user, false)
+    elsif self.is_a?(TlThread)
+      self.hash_without_seams(user)
+    else
+      self.as_json
+    end
+  end
+
+  private
+
+  def self.indifferent_hash(hash)
+    HashWithIndifferentAccess.new.merge(hash)
+  end
+
+  def prevent_caching?
+    if self.prevent_caching
+      self.prevent_caching = false
+      return true
+    end
+    false
+  end
+
+end
